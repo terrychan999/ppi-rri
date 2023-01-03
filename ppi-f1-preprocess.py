b63ce1a173fdbaf89e46302b69d99779f3ff05dd
Terry Chan <terrychan999@gmail.com>
2023-01-03 09:49:17 +0800

    update 1. ppi-f1-preprocess.py 2. .gitignore

--- ppi-f1-preprocess.py
+++ ppi-f1-preprocess.py
@@ -1,24 +1,29 @@
-from Bio import SeqIO
+"""convert fasta to txt"""
+
+from Bio import SeqIO  # requires biopython
 import os
 
-# convert fasta to txt
 
-# current directory path
-currentPath = os.getcwd()
+currentPath = os.getcwd()  # current directory path
 databasesPath = os.path.join(currentPath, "databases")
 proteinFastaName = os.path.join(databasesPath, "uniprot_sprot.fasta.gz")
-convertTxtName = os.path.join(databasesPath, "swissProtSeq.txt")
+convertTxtName = os.path.join(databasesPath, "uniprotSeq.txt")
 
-if ".gz" in proteinFastaName:
-    import gzip
-    handle = gzip.open(proteinFastaName, "rt")
-else:
-    handle = open(proteinFastaName, "r")
-seqRecords = SeqIO.parse(handle, "fasta")
 
-swissProtSeq = open(convertTxtName, "w")
+def load_fasta(fasta_file):  # load fasta directly or with compression
+    if ".gz" in fasta_file:
+        import gzip
+        handle = gzip.open(fasta_file, "rt")
+    else:
+        handle = open(fasta_file, "r")
+    return SeqIO.parse(handle, "fasta")
+
+
+seqRecords = load_fasta(proteinFastaName)
+
+# write to txt
+uniprotSeq = open(convertTxtName, "w")
 for index, record in enumerate(seqRecords):
     accession_id = record.id.split("|")[1]
-    swissProtSeq.write(f"{accession_id} {record.seq}\n")
-
-swissProtSeq.close()
+    uniprotSeq.write(f"{accession_id}\t{record.seq}\n")
+uniprotSeq.close()
