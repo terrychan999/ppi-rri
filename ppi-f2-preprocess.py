3bdafe47fe31c39f0342533021f7d4b1d7c7162c
Terry Chan <terrychan999@gmail.com>
2023-01-03 14:07:03 +0800

    update ppi-f2-preprocess.py

--- ppi-f2-preprocess.py
+++ ppi-f2-preprocess.py
@@ -15,17 +15,25 @@ DAG = dict()
 for line in goLines:
     if "GO:" in line:
         if line.startswith("id:"):
-            curr_id = line.split(": ")[1].strip()
+            curr_id = line.split(" ")[1].strip()
             if curr_id not in DAG:
                 DAG[curr_id] = list()
         elif line.startswith("is_a:"):
-            is_a = line.split(": ")[1].split(" !")[0].strip()
+            is_a = line.split(" ")[1].strip()
             if is_a not in DAG:
                 DAG[is_a] = list()
                 DAG[is_a].append(curr_id)
             else:
                 if curr_id not in DAG[is_a]:
                     DAG[is_a].append(curr_id)
+        elif line.startswith("relationship: part_of") or line.startswith("intersection_of: part_of"):
+            part_of = line.split(" ")[2].strip()
+            if part_of not in DAG:
+                DAG[part_of] = list()
+                DAG[part_of].append(curr_id)
+            else:
+                if curr_id not in DAG[part_of]:
+                    DAG[part_of].append(curr_id)
 
 """write to txt"""
 directedGraph = open(convertTxtName, "w")
