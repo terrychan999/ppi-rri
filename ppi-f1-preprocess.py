c07566369731c01243dcb5e56b7ecd283b3df05d
Terry Chan <terrychan999@gmail.com>
2023-02-23 20:00:30 +0800

    update f1 preprocess

--- ppi-f1-preprocess.py
+++ ppi-f1-preprocess.py
@@ -8,10 +8,11 @@ currentPath = os.getcwd()  # current directory path
 databasesPath = os.path.join(currentPath, "databases")
 proteinFastaName = os.path.join(databasesPath, "uniprot_sprot.fasta.gz")
 convertTxtName = os.path.join(databasesPath, "uniprotSeq.txt")
+seqBasedTxtName = os.path.join(databasesPath, "seqBased.txt")
 
 
 def load_fasta(fasta_file):  # load fasta directly or with compression
-    if ".gz" in fasta_file:
+    if fasta_file.endswith(".gz"):
         import gzip
         handle = gzip.open(fasta_file, "rt")
     else:
@@ -21,9 +22,30 @@ def load_fasta(fasta_file):  # load fasta directly or with compression
 
 seqRecords = load_fasta(proteinFastaName)
 
-# write to txt
+"""write to txt"""
 uniprotSeq = open(convertTxtName, "w")
 for index, record in enumerate(seqRecords):
     accession_id = record.id.split("|")[1]
     uniprotSeq.write(f"{accession_id}\t{record.seq}\n")
 uniprotSeq.close()
+
+"""write to txt with seq as key"""
+# seqBased = open(seqBasedTxtName, "w")
+# seqDict = dict()
+# for index, record in enumerate(seqRecords):
+#     accession_id = record.id.split("|")[1]
+#     if record.seq not in seqDict:
+#         seqDict[record.seq] = list()
+#         seqDict[record.seq].append(accession_id)
+#     else:
+#         seqDict[record.seq].append(accession_id)
+# for seq, accession_ids in seqDict.items():
+#     seqBased.write(f"{seq}\t{','.join(accession_ids)}\n")
+# seqBased.close()
+
+"""count human proteins"""
+# human_count = 0
+# for index, record in enumerate(seqRecords):
+#     if "_HUMAN" in record.id:
+#         human_count += 1
+# print(human_count)
